23af85e9ef97f6b952b2e6a8520d7ab434d817a1
Mohamed K Sulaiman <sulaimanhamsa555@gmail.com>
2024-05-26 17:11:58 +0530

    Basic setups

--- miniurl/api/views.py
+++ miniurl/api/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
